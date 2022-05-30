2fc49e827c30610a4291bb965c45368354742d2d
HariboTube <57761676+HariboTube@users.noreply.github.com>
2022-05-30 09:38:02 +0300

    Update botTesla.js

--- botTesla.js
+++ botTesla.js
@@ -446,7 +446,7 @@ bot.action('dndjd', ctx=>{
         reply_markup:{
             inline_keyboard:[
                 [{text:'Botu Grupa Ekle ✅', url:`t.me/${process.env.BOT_ISMI}?startgroup=true`}],
-                [{text:'Resmi Kanalımız 📣', url:`t.me/teslagametr`},{text:'VİP Gruplar 💎', callback_data:'vip'}]
+                [{text:'Resmi Kanalımız 📣', url:`t.me/zamanimiz`},{text:'VİP Gruplar 💎', callback_data:'vip'}]
             ]
         }
     })
